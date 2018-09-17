587f3d544e49027cb4d60caa7af7d5b546e4077a
Annie Eby <eby.annie@gmail.com>
2018-09-17 20:35:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-  if (number %3 == 0)
+def fizzbuzz(number)
+number
+  if (number %15 == 0)
+    print "FizzBuzz"
+  elsif (number %3 == 0)
+    print "Fizz"
+  elsif (number %5 == 0)
+    print "Buzz"
+  else 
+    print "nil"
 end
